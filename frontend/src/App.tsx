7494104e55e0eab1fdf024f3816aa3cf37b63d66
Cezar Augusto <cezar.ags@icloud.com>
2021-05-04 21:31:19 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,24 @@
+import DataTable from "components/DataTable";
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+import React from "react";
 
 
 
 function App() {
   return (
-    <div>
+    <>
+    <NavBar/>
+    <div className="container"> 
       <h1 className="text-primary">Olá, Mundo</h1>
-      
-    </div>
+      <DataTable/>
+    </div> 
+    
+    <Footer/>
+  
+    </>
+
+    
   );
 }
 
