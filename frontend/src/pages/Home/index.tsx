de629956508d6abc5684df9700cec14ca4c3c5c2
Cezar Augusto <cezar.ags@icloud.com>
2021-05-09 19:35:56 -0300

    Home, Navigation

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,9 +1,23 @@
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+import React from "react";
+import { Link } from "react-router-dom";
+
 function Home() {
     return (
-    <div>
-        <h2>Pagina Home</h2>
-
+        <>
+        <NavBar/>
+        <div className="container">
+        <div className="jumbotron">
+            <h1 className="display-4">DJVendas</h1>
+            <p className="lead">Analise o desempenho das suas vendas por diferentes perspectivas</p>
+            <hr/>
+            <p>Esta aplicação consiste em exibir um dashboard a partir de dados fornecidos por um back end construído com Spring Boot.</p>
+        <Link className="btn btn-primary btn-lg" to="/dashboard">Acessar o Dashboard</Link>
+        </div>
     </div>
+    <Footer/>
+    </>
        
     );
   }
